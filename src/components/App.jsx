37b474c90c93983adfbc2cc08c3f1962a20f13ea
VladyslavPunko <143653897+VladyslavPunko@users.noreply.github.com>
2024-04-17 21:49:52 +0200

    /

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,62 +1,16 @@
 import "./App.css";
-import { useEffect, useState } from "react";
 import ContactList from "./ContactList/ContactList";
 import SearchBox from "./SearchBox/SearchBox";
 import ContactForm from "./ContactForm/ContactForm";
-import { nanoid } from "nanoid";
-
-const initialContacts = [
-  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-];
 
 function App() {
-  const [contacts, setContacts] = useState(() => {
-    const savedContacts = window.localStorage.getItem("saved-Contacts");
-    if (savedContacts !== null) {
-      return JSON.parse(savedContacts);
-    }
-    return initialContacts;
-  });
-
-  useEffect(() => {
-    window.localStorage.setItem("saved-Contacts", JSON.stringify(contacts)),
-      [contacts];
-  });
-
-  const [filter, setFilter] = useState("");
-
-  const onChangeFilter = (event) => {
-    setFilter(event.target.value);
-  };
-
-  const addContact = (newContact) => {
-    const finalContact = { ...newContact, id: nanoid() };
-    setContacts((prevContacts) => [...prevContacts, finalContact]);
-  };
-
-  const filteredContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
-
-  const onDeleteContact = (contactId) => {
-    setContacts((prevContacts) =>
-      prevContacts.filter((contact) => contact.id !== contactId)
-    );
-  };
-
   return (
     <div>
       <h1>Phonebook</h1>
-      <ContactForm addContact={addContact} />
-      <SearchBox filter={filter} onChangeFilter={onChangeFilter} />
+      <ContactForm />
+      <SearchBox />
 
-      <ContactList
-        contacts={filteredContacts}
-        onDeleteContact={onDeleteContact}
-      />
+      <ContactList />
     </div>
   );
 }
