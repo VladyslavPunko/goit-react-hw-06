6e3edd42ec47275d5119e7e264524734f9405f3c
VladyslavPunko <143653897+VladyslavPunko@users.noreply.github.com>
2024-04-17 22:04:48 +0200

    s

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
-import ContactList from "./ContactList/ContactList.js";
-import SearchBox from "./SearchBox/SearchBox.jsx";
-import ContactForm from "./ContactForm/ContactForm.jsx";
+import ContactList from "./ContactList/ContactList";
+import SearchBox from "./SearchBox/SearchBox";
+import ContactForm from "./ContactForm/ContactForm";
 
 function App() {
   return (
